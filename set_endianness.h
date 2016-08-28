317314a04dd091768feef53d8656a3381939a1bc
circulosmeos <loopidle@gmail.com>
2016-08-28 18:11:12 +0200

    code automated and simplified for endianness and windows platforms detection

--- set_endianness.h
+++ set_endianness.h
@@ -1,34 +1,50 @@
 #ifndef SET_ENDIANNESS_H
 #define SET_ENDIANNESS_H
 
-#ifdef ANDROID_LIBRARY
+//#ifdef ANDROID_LIBRARY
 /*
    __BIG_ENDIAN__ and __LITTLE_ENDIAN__ are defined in some gcc versions
   only, probably depending on the architecture. Try to use endian.h if
   the gcc way fails - endian.h also does not seem to be available on all
   platforms.
 */
+
+// http://esr.ibiblio.org/?p=5095
+// http://stackoverflow.com/questions/2100331/c-macro-definition-to-determine-big-endian-or-little-endian-machine
+
+/* definition to expand macro then apply to pragma message */
+// http://stackoverflow.com/questions/1562074/how-do-i-show-the-value-of-a-define-at-compile-time
+/*#define VALUE_TO_STRING(x) #x
+#define VALUE(x) VALUE_TO_STRING(x)
+#define VAR_NAME_VALUE(var) #var "="  VALUE(var)*/
+
 #ifdef __BIG_ENDIAN__
-#undef LOCAL_LITTLE_ENDIAN
-#else /* __BIG_ENDIAN__ */
-#ifdef __LITTLE_ENDIAN__
-#define LOCAL_LITTLE_ENDIAN
+#	undef LOCAL_LITTLE_ENDIAN
 #else
-#ifdef BSD
-#include <sys/endian.h>
-#else
-#include <endian.h>
-#endif
-#if __BYTE_ORDER == __BIG_ENDIAN
-#undef LOCAL_LITTLE_ENDIAN
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
-#define LOCAL_LITTLE_ENDIAN
-#else
-#error "unable to determine endianess!"
-#endif /* __BYTE_ORDER */
-#endif /* __LITTLE_ENDIAN__ */
+#	ifdef __LITTLE_ENDIAN__
+#		define LOCAL_LITTLE_ENDIAN
+#	else
+#		ifdef BSD
+#			include <sys/endian.h>
+#		elif defined(__linux__)
+#			include <endian.h>
+#		endif /* BSD */
+#		if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __BIG_ENDIAN__
+#			undef LOCAL_LITTLE_ENDIAN
+#		elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __LITTLE_ENDIAN__
+#			define LOCAL_LITTLE_ENDIAN
+#		elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#			undef LOCAL_LITTLE_ENDIAN
+#		elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#			define LOCAL_LITTLE_ENDIAN
+#		else
+#			error "unable to determine endianess!"
+#		endif /* __BYTE_ORDER == __BIG_ENDIAN */
+#	endif /* __LITTLE_ENDIAN__ */
 #endif /* __BIG_ENDIAN__ */
 
-#endif /* ANDROID_LIBRARY */
+/*#pragma message(VAR_NAME_VALUE(LOCAL_LITTLE_ENDIAN))*/
+
+//#endif /* ANDROID_LIBRARY */
 
 #endif
--- triops.h
+++ triops.h
@@ -8,41 +8,81 @@
 #ifndef TRIOPS_H_
 #define TRIOPS_H_
 
+
 // .................................................
 // large file support (LFS) (files with size >2^31 (2 GiB) in linux, and >4 GiB in Windows)
-#define _FILE_OFFSET_BITS 64	// stat, fseek
+#define _FILE_OFFSET_BITS 64    // stat, fseek
 #define _LARGEFILE_SOURCE
-#define _LARGEFILE64_SOURCE		// off64_t for fseek64
-#define ZERO_LL 0LL				// long long zero - crafted specially to be used in FSEEK( , , SEEK_END);
+#define _LARGEFILE64_SOURCE     // off64_t for fseek64
+#define ZERO_LL 0LL             // long long zero - crafted specially to be used in FSEEK( , , SEEK_END);
 // .................................................
 
 // .................................................
-#define WINDOWS_PLATFORM 	// Compile for Unix or for Windows: #undef o #define
-							// this just includes (*nix) or don't (windows) "windef.h"
-#define LOCAL_LITTLE_ENDIAN	// it is important to undef in order to compile
-							// on Big Endian processors (p. ej. SPARC, or Itanium with HP-UX)
+// Endianness determination: 
+// Little Endian processors (Intel/AMD x86 & x86_64, or ARM ...) 
+// Big Endian processors (SPARC (with Solaris), or Itanium with HP-UX ...)
+#include "set_endianness.h" // LOCAL_LITTLE_ENDIAN is automagically determined if ANDROID_LIBRARY is set
+                            // as the library can, and will, be compiled for multiple plataforms.
+//#define LOCAL_LITTLE_ENDIAN // replaced by (hopefully) automatic determination in "set_endianness.h"
+                              // but if set_endianness.h fails, it can be set (define/undef) here.
 // .................................................
 
-#undef ANDROID_LIBRARY 		// avoid stdin and stdout manipulations, as this will be an OS library (.so)
 
-#include "set_endianness.h"	// LOCAL_LITTLE_ENDIAN is automagically determined if ANDROID_LIBRARY is set
- 							// as the library can, and will, be compiled for multiple plataforms.
+// .................................................
+// If defined, avoid stdin and stdout manipulations, as this will be an OS library (.so)
+// and replace main() with a JNI-aware function
+#undef ANDROID_LIBRARY
+// .................................................
+
+
 
 //#include <stdint.h>
 // v6: Solaris compatibility
 #include <inttypes.h>
 
 
-#ifndef WINDOWS_PLATFORM
-#include "windef.h" // Windows types definitions
-#include <utime.h>  // to change the modification time (and so to preverve the original one)
-#define FSEEK fseeko 	// (LFS)
+#ifndef _WIN32
+
+#   undef WINDOWS_PLATFORM
+
+#   define MAX_PATH          260
+
+#   ifndef NULL
+#       define NULL    ((void *)0)
+#   endif
+
+#   ifndef FALSE
+#       define FALSE               0
+#   endif
+
+#   ifndef TRUE
+#       define TRUE                1
+#   endif
+
+    // 64 bit compatibility
+    typedef uint32_t            DWORD;
+    typedef uint16_t            WORD;
+
+    typedef int                 BOOL;
+    typedef unsigned char       BYTE;
+    typedef BYTE                *LPBYTE;
+    typedef DWORD               *LPDWORD;
+    typedef char                *LPSTR;
+
+#   include <utime.h>   // to change the modification time (and so to preverve the original one)
+#   define FSEEK fseeko // (LFS)
+
 #else
-#include <windows.h>
-#include <winbase.h> // to use GetFileTime and SetFileTime (on Unix I use utime)
-#include <io.h>		 // _chsize_s, _open (LFS)
-#define FSEEK _fseeki64 // large file support in windows (LFS)
+
+#   define WINDOWS_PLATFORM
+
+#   include <windows.h>
+#   include <winbase.h>     // to use GetFileTime and SetFileTime (on Unix I use utime)
+#   include <io.h>          // _chsize_s, _open (LFS)
+#   define FSEEK _fseeki64  // large file support in windows (LFS)
+
 #endif
+
 
 #include <string.h>
 #include <fcntl.h>  // open, etc.
@@ -51,18 +91,18 @@
 #include <ctype.h>  // isprint()
 
 #ifndef __sun
-#include <getopt.h> // getopt() compatible with -std=c99
+#   include <getopt.h> // getopt() compatible with -std=c99
 #endif
 
 // sets binary mode for stdin in Windows
 #define STDIN 0
 #define STDOUT 1
 #ifdef _WIN32
-# include <io.h>
-# include <fcntl.h>
-# define SET_BINARY_MODE(handle) setmode(handle, O_BINARY)
+#   include <io.h>
+#   include <fcntl.h>
+#   define SET_BINARY_MODE(handle) setmode(handle, O_BINARY)
 #else
-# define SET_BINARY_MODE(handle) ((void)0)
+#   define SET_BINARY_MODE(handle) ((void)0)
 #endif
 
 
